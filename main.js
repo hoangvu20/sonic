429825fc70f5b9007c784dad587217425ee3f462
hoangvu20 <hoangvu7239@gmail.com>
2024-10-04 21:30:43 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const index1 = require("./index1");
+const index2 = require("./index2");
+const index3 = require("./index3");
+const index4 = require("./index4");
+const index5 = require("./index5");
+const index6 = require("./index6");
+const index7 = require("./index7");
+const index8 = require("./index8");
+const index9 = require("./index9");
+const index10 = require("./index10");
+
+index1;
+index2;
+index3;
+index4;
+index5;
+index6;
+index7;
+index8;
+index9;
+index10;
